3a9014c7ab6941af9460bc4d97d47d34a1b198d4
Kevin Wood <kwood105@wgu.edu>
2024-10-09 14:39:27 -0500

    Added comments for clarity on what the program is.

--- hamburger.py
+++ hamburger.py
@@ -1,8 +1,19 @@
+
+#this function accepts the amount of hamburgers requested by the user and
+#gives them those hamburgers 
+
 def getHamburgers(amount):
     print(f'Here are {amount} hamburgers')
     print('Thank you for stopping by')
 
+#first, getting the user's answer for what they want. We hope
+#that it's hamburgers. At least I do. 
+
 want = input("What do you want?: ")
+
+#if the user wants hamburgers, we ask them how many
+#and then give them the hamburgers they asked for. If not,
+#we ridicule them.
 
 if want == "hamburgers":
     amount = int(input('How many?: '))
