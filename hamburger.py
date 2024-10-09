904647bb3477c3afe564c986d870df5e90d723ca
Kevin Wood <kwood105@wgu.edu>
2024-10-09 14:29:20 -0500

    here's the hamburger file

---
+++ hamburger.py
@@ -0,0 +1,11 @@
+def getHamburgers(amount):
+    print(f'Here are {amount} hamburgers')
+    print('Thank you for stopping by')
+
+want = input("What do you want?: ")
+
+if want == "hamburgers":
+    amount = int(input('How many?: '))
+    getHamburgers(amount)
+else:
+    print("We don't have those.")
